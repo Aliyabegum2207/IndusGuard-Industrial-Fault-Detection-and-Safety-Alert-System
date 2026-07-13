25bcc3fdbf3494be33448d8f329748fda1b4e4fa
Aliyabegum2207 <aliyabegum2207@gmail.com>
2026-07-13 14:49:51 +0530

    Update exint.c

--- exint.c
+++ exint.c
@@ -1,51 +1,67 @@
 #include <LPC21xx.h>
 #include "types.h"
-//#include "pin_function_defines.h"
-//#include "pin_connect_block.h"
 
+/* External Interrupt (EINT0) Configuration */
 #define EINTO_VIC_CHNO 14
 #define EINTO_STATUS_LED 21
 #define EINTO_PIN_0_1 1
 
+/* External Interrupt Service Routine Prototype */
 void einto_isr(void) __irq;
+
+/* Flag used to indicate that external interrupt has occurred */
 u32 i_flag=0;
+
+/*---------------------------------------------------------------
+ Function : Enable_int()
+ Purpose  : Configures External Interrupt 0 (EINT0).
+            - Configures status LED as output
+            - Configures P0.1 as EINT0 input
+            - Enables EINT0 interrupt through VIC
+            - Configures interrupt as Edge Triggered
+----------------------------------------------------------------*/
 void Enable_int(void)
 {
-	//cfg EINTO status led pin as gpio output
+	/* Configure status LED pin as GPIO output */
 	IODIR0|=1<<EINTO_STATUS_LED;
-	//cfg p0.1 pin as EINTO input pin
-	//cfgPortFunc(0,16,EINTO_PIN_0_1);
-	PINSEL1|=0x15400001; 
-	//PINSEL1|=0x15400000;
-	//cfg VIC peripheral for EINTO
-	//cfg EINTO as irq type,def all are anyway irq
-	//VICINTSelect=0;
-	//Enable EINTO via VIC
+
+	/* Configure P0.1 pin for EINT0 function */
+	PINSEL1|=0x15400001;
+
+	/* Enable EINT0 interrupt in VIC */
 	VICIntEnable=1<<EINTO_VIC_CHNO;
-	//cfg EINTO as v.irq,highest priority 0
+
+	/* Configure EINT0 as Vectored IRQ with high priority */
 	VICVectCntl1=(1<<5)|EINTO_VIC_CHNO;
-	//load einto_isr addr in hardware lut sfr for v.irq
+
+	/* Load ISR address into VIC vector table */
 	VICVectAddr1=(u32)einto_isr;
-	//cfg External interrupt peripheral
-	//Enable EINTO,def all eints are any enable
-	//EXTINT=0;
-	//cfg EINTO for edge triggering
+
+	/* Configure EINT0 as Edge Triggered */
 	EXTMODE=1<<0;
-	//cfg EINTO for falling edge triggering,def FE anyway
-	//EXTPOLAR=0
-	//enter regular operations
-}
-void einto_isr(void) __irq
-{
-	//einto isr user activity begins
-	//toggle EINTO status led upon interrupt fired/raised
-	IOPIN0^=1<<EINTO_STATUS_LED;
-	i_flag=1;
-	  
-	//einto isr user activity ends
-	//clear EINTO status in external interrupt perpheral
-	EXTINT=1<<0;
-	//clear EINTO status in peripheral
-	VICVectAddr=0;
+
+	/* Falling edge triggering is default for EINT0 */
 }
 
+
+/*---------------------------------------------------------------
+ Function : einto_isr()
+ Purpose  : Interrupt Service Routine for External Interrupt 0.
+            - Toggles status LED
+            - Sets interrupt flag
+            - Clears interrupt status
+----------------------------------------------------------------*/
+void einto_isr(void) __irq
+{
+	/* Toggle status LED whenever interrupt occurs */
+	IOPIN0^=1<<EINTO_STATUS_LED;
+
+	/* Set interrupt flag for main application */
+	i_flag=1;
+
+	/* Clear EINT0 interrupt flag */
+	EXTINT=1<<0;
+
+	/* Notify VIC that interrupt servicing is complete */
+	VICVectAddr=0;
+}
